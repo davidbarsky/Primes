168d13fddfc9ef84797daca403896f2b69c82b8a
David Barsky <barskydavid@gmail.com>
2014-08-04 09:30:03 -0400

    removed 0 from tile generators

--- Source/Tile.m
+++ Source/Tile.m
@@ -19,7 +19,7 @@
 
 - (id)init {
     if (self = [super init]) {
-        self.value = [NSNumber numberWithUnsignedInt:arc4random()%10];
+        self.value = [NSNumber numberWithUnsignedInt:arc4random_uniform(4) + 5];
     }
     return self;
 }
